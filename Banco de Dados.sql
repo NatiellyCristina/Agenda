2b0863a94dd9a7273cae9171283a3aa6cc3a5031
NatiellyCristina <53455401+NatiellyCristina@users.noreply.github.com>
2020-06-11 15:50:42 -0300

    Update Banco de Dados.sql

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -6,12 +6,12 @@ CREATE DATABASE if not exists AGENDA;
  USE AGENDA;
  
  CREATE TABLE IF NOT EXISTS contato (
-    id_contato INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_contato INT NOT NULL PRIMARY,
     nome VARCHAR(25),
     endereco varchar(25)
 );
  CREATE TABLE IF NOT EXISTS telefone(
-    id_telefone INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_telefone INT NOT NULL PRIMARY KEY,
     id_contato INT NOT NULL,
     telefone varchar(25),
         FOREIGN KEY (id_contato)
